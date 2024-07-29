24787b7c220aee0a3f28ac66b5d87f0c9e43ceb1
NitdherG <nitdherdev@gmail.com>
2024-07-29 15:05:17 -0400

    [backend-todo] se agrega las rutas y se configura un puerto para ejecutar localmente

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { onRequest } from "firebase-functions/v2/https";
 import express from "express";
+import userRoutes from "./interfaces/routes/userRoutes";
+import taskRoutes from "./interfaces/routes/taskRoutes";
 
 const app = express();
 app.use(express.json());
+app.use("/api", taskRoutes);
+app.use("/api", userRoutes);
+
 // Configurar puerto
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 export const api = onRequest(app);
