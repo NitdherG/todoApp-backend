63f9d61fa735f79684e79e6eb4c5bf2b20dd0113
NitdherG <nitdherdev@gmail.com>
2024-07-29 11:02:42 -0400

    [backend-todo] Se agrega la configuracion de acceso a la bd

---
+++ src/infrastructure/firestore/FirestoreConfig.ts
@@ -0,0 +1,17 @@
+import * as admin from "firebase-admin";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+if (!serviceAccountPath) {
+  throw new Error("GOOGLE_APPLICATION_CREDENTIALS must be set");
+}
+
+const serviceAccount = require("../../../" + serviceAccountPath);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
+export { db, admin };
