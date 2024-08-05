a4284470aa04511a4c924b3950dd2627f6846155
NitdherG <nitdherdev@gmail.com>
2024-08-05 00:05:22 -0400

    [backend-todo] se agregan comentarios

--- src/app/Use-cases/User/AddUserUseCase.ts
+++ src/app/Use-cases/User/AddUserUseCase.ts
@@ -1,9 +1,28 @@
+
 import { User, UserRepository } from "../../../domain";
 
+/**
+ * Use case for adding a new user.
+ * This class handles the business logic for adding a user to the repository.
+ */
 export class AddUserUseCase {
+  /**
+   * Creates an instance of the AddUserUseCase.
+   * @param userRepository - The repository to interact with user data.
+   */
   constructor(private userRepository: UserRepository) {}
 
+  /**
+   * Executes the use case to add a new user.
+   * @param user - The user object to be added to the repository.
+   * @returns A promise that resolves when the user has been added.
+   * @throws {Error} - Throws an error if the operation fails.
+   * @remarks
+   * This method calls the `addUser` method of the `UserRepository` to persist the user data.
+   * No value is returned upon successful completion.
+   */
   async execute(user: User): Promise<void | null> {
     await this.userRepository.addUser(user);
   }
 }
+
--- src/app/Use-cases/User/GetUserUseCase.ts
+++ src/app/Use-cases/User/GetUserUseCase.ts
@@ -1,9 +1,26 @@
 import { User, UserRepository } from "../../../domain";
 
+/**
+ * Use case for retrieving a user by their email.
+ * This class handles the business logic for fetching a user from the repository.
+ */
 export class GetUserUseCase {
+  /**
+   * Creates an instance of the GetUserUseCase.
+   * @param userRepository - The repository to interact with user data.
+   */
   constructor(private userRepository: UserRepository) {}
 
-  async execute(id: string): Promise<User | null> {
-    return await this.userRepository.getUser(id);
+  /**
+   * Executes the use case to retrieve a user by their email.
+   * @param email - The email of the user to be retrieved.
+   * @returns A promise that resolves to the user object if found, or null if not found.
+   * @throws {Error} - Throws an error if the operation fails.
+   * @remarks
+   * This method calls the `getUser` method of the `UserRepository` to fetch the user data.
+   * The return value is a `User` object if the user is found, otherwise, it returns `null`.
+   */
+  async execute(email: string): Promise<User | null> {
+    return await this.userRepository.getUser(email);
   }
 }
