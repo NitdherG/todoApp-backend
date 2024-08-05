975f2b2fa68fdfd61efb179398be55d59bc922c4
NitdherG <nitdherdev@gmail.com>
2024-08-05 00:05:05 -0400

    [backend-todo] se actualiza de acuerdo a las pruebas y al uso que se aplica

--- src/infrastructure/firestore/FirestoreTaskRepository.ts
+++ src/infrastructure/firestore/FirestoreTaskRepository.ts
@@ -1,28 +1,124 @@
 import { Task, TaskRepository } from "../../domain";
-import { db } from "./FirestoreConfig";
+import { CustomError } from "../../domain/errors/custom.error";
+import { admin, db } from "./FirestoreConfig";
+import { Timestamp } from "@google-cloud/firestore";
 
+/**
+ * FirestoreTaskRepository is an implementation of TaskRepository that uses Firestore as the data store.
+ */
 export class FirestoreTaskRepository implements TaskRepository {
-  async addTask(task: Task): Promise<void> {
-    await db.collection("tasks").add({
-      id: task.id,
-      name: task.name,
-      completed: task.completed,
-      createdAt: task.createdAt,
-    });
+  /**
+   * Adds a task to the specified user's task collection.
+   * @param task - The task to be added.
+   * @param userId - The ID of the user to whom the task belongs.
+   * @throws {CustomError} - If the task cannot be added.
+   */
+  async addTask(task: Task, userId: string): Promise<void> {
+    try {
+      console.log("task", task);
+      await db.collection(`users/${userId}/tasks`).add({
+        name: task.name,
+        completed: task.completed,
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        description: task.description,
+      });
+    } catch (error) {
+      throw CustomError.badRequest("No se pudo agregar la tarea");
+    }
   }
-  async getTasks(): Promise<Task[]> {
-    const snapshot = await db.collection("tasks").get();
-    return snapshot.docs.map(
-      (doc) =>
-        new Task(
+
+  /**
+   * Retrieves all tasks for a specified user with pagination.
+   * @param userId - The ID of the user whose tasks are to be retrieved.
+   * @param limit - The maximum number of tasks to retrieve.
+   * @param startAfter - The ID of the last task from the previous page.
+   * @returns An object containing an array of tasks and the total count of tasks.
+   * @throws {CustomError} - If the tasks cannot be retrieved.
+   */
+  async getTasks(
+    email: string,
+    limit: number,
+    startAfter?: string,
+  ): Promise<{ tasks: Task[]; totalCount: number }> {
+    try {
+      let query = db
+        .collection(`users/${email}/tasks`)
+        .orderBy("createdAt", "desc")
+        .limit(limit);
+
+      if (startAfter) {
+        const lastDoc = await db
+          .collection(`users/${email}/tasks`)
+          .doc(startAfter)
+          .get();
+        query = query.startAfter(lastDoc);
+      }
+
+      const snapshot = await query.get();
+      const tasks = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        let createdAt: Date | undefined = undefined;
+
+        // Verificar si createdAt es un Timestamp y convertirlo a Date
+        if (data.createdAt instanceof Timestamp) {
+          createdAt = data.createdAt.toDate();
+        }
+
+        // Manejar el caso cuando createdAt no está presente
+        if (!createdAt && data.createdAt !== "") {
+          console.warn(`Invalid createdAt value for task: ${doc.id}`);
+        }
+
+        return new Task(
           doc.id,
-          doc.data().name,
-          doc.data().completed,
-          doc.data().createdAt.toDate(),
-        ),
-    );
+          data.name,
+          data.completed,
+          createdAt,
+          data.description,
+        );
+      });
+
+      // Obtener el total de ítems
+      const totalSnapshot = await db.collection(`users/${email}/tasks`).get();
+      const totalCount = totalSnapshot.size;
+
+      return { tasks, totalCount };
+    } catch (error) {
+      throw CustomError.notFound("No se encontró el usuario con email");
+    }
   }
-  async deleteTask(id: string): Promise<void> {
-    await db.collection("tasks").doc(id).delete();
+
+  async updateTask(task: Task, email: string): Promise<void> {
+    try {
+      await db.collection(`users/${email}/tasks`).doc(task.id).update({
+        name: task.name,
+        completed: task.completed,
+        description: task.description,
+      });
+    } catch (error) {
+      throw CustomError.badRequest("No se pudo actualizar la tarea");
+    }
+  }
+  /**
+   * Deletes a task by its ID.
+   * @param id - The ID of the task to be deleted.
+   * @throws {CustomError} - If the task cannot be deleted.
+   */
+
+  async deleteTask(email: string, id: string): Promise<void> {
+    try {
+      const docRef = db.collection(`users/${email}/tasks`).doc(id);
+
+      const doc = await docRef.get();
+
+      if (!doc.exists) {
+        throw CustomError.badRequest("La tarea no existe");
+      }
+
+      await docRef.delete();
+    } catch (error) {
+      console.error("Error al eliminar la tarea:", error);
+      throw CustomError.badRequest("No se pudo borrar la tarea");
+    }
   }
 }
--- src/infrastructure/firestore/FirestoreUserRepository.ts
+++ src/infrastructure/firestore/FirestoreUserRepository.ts
@@ -1,22 +1,53 @@
 import { User, UserRepository } from "../../domain";
+import { CustomError } from "../../domain/errors/custom.error";
 import { admin, db } from "./FirestoreConfig";
 
+/**
+ * FirestoreUserRepository is an implementation of UserRepository that uses Firestore as the data store.
+ */
 export class FirestoreUserRepository implements UserRepository {
+
+  /**
+   * Adds a user to the users collection.
+   * @param user - The user to be added.
+   * @throws {CustomError} - If the user cannot be added.
+   */
   async addUser(user: User): Promise<void> {
-    await db.collection("users").add({
-      name: user.name,
-      email: user.email,
-      createdAt: admin.firestore.FieldValue.serverTimestamp(),
-    });
+    try {
+      await db.collection("users").add({
+        email: user.email,
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      });
+    } catch (error) {
+      throw CustomError.badRequest("No se pudo agregar el usuario");
+    }
   }
 
-  async getUser(id: string): Promise<User | null> {
-    const doc = await db.collection("users").doc(id).get();
-    if (!doc.exists) {
-      return null;
+  /**
+   * Retrieves a user by their email.
+   * @param email - The email of the user to be retrieved.
+   * @returns The user if found, or null if not found.
+   * @throws {CustomError} - If the user cannot be found.
+   */
+  async getUser(email: string): Promise<User | null> {
+    try {
+      const querySnapshot = await db
+        .collection("users")
+        .where("email", "==", email)
+        .get();
+      if (querySnapshot.empty) {
+        console.log("No se encontró el usuario con email:", email);
+        throw CustomError.notFound("No se encontró el usuario");
+      }
+      const doc = querySnapshot.docs[0];
+
+      return new User(
+        doc.id,
+        doc.data().email,
+        doc.data().createdAt,
+      );
+    } catch (error) {
+      throw CustomError.notFound("No se encontró el usuario");
     }
-    const data = doc.data();
-    return new User(doc.id, data!.name, data!.email, data!.createdAt.toDate());
   }
-  
 }
