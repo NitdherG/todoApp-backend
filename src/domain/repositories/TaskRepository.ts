89de4c866d21489650d9aabe8931664c3bbf9f6a
NitdherG <nitdherdev@gmail.com>
2024-08-05 00:02:44 -0400

    [backend-todo] se agrega comentarios a los repositorios

--- src/domain/repositories/TaskRepository.ts
+++ src/domain/repositories/TaskRepository.ts
@@ -1,7 +1,34 @@
 import { Task } from "../entities/Task";
 
+/**
+ * Defines the contract for a repository that manages tasks.
+ */
 export interface TaskRepository {
-  addTask(task: Task): Promise<void>;
-  getTasks(): Promise<Task[]>;
-  deleteTask(id: string): Promise<void>;
+  /**
+   * Adds a new task for a specific user.
+   * @param task - The task to be added.
+   * @param userId - The unique identifier of the user to whom the task belongs.
+   * @returns A promise that resolves when the task has been added.
+   */
+  addTask(task: Task, userId: string): Promise<void>;
+
+  /**
+   * Retrieves all tasks for a specific user.
+   * @param userId - The unique identifier of the user whose tasks are to be retrieved.
+   * @returns A promise that resolves to an array of tasks for the user.
+   */
+  getTasks(
+    userId: string,
+    limit: number,
+    startAfter?: string,
+  ): Promise<{ tasks: Task[]; totalCount: number }>;
+
+  /**
+   * Deletes a specific task by its ID.
+   * @param id - The unique identifier of the task to be deleted.
+   * @returns A promise that resolves when the task has been deleted.
+   */
+  deleteTask(email: string, id: string): Promise<void>;
+
+  updateTask(task: Task, userId: string): Promise<void>;
 }
--- src/domain/repositories/UserRepository.ts
+++ src/domain/repositories/UserRepository.ts
@@ -1,6 +1,20 @@
 import { User } from "../entities/User";
 
+/**
+ * Defines the contract for a repository that manages users.
+ */
 export interface UserRepository {
+  /**
+   * Adds a new user to the repository.
+   * @param user - The user to be added.
+   * @returns A promise that resolves when the user has been added.
+   */
   addUser(user: User): Promise<void>;
-  getUser(id: string): Promise<User | null>;
+
+  /**
+   * Retrieves a user by their email.
+   * @param email - The email address of the user to be retrieved.
+   * @returns A promise that resolves to the user if found, or null if no user is found with the specified email.
+   */
+  getUser(email: string): Promise<User | null>;
 }
