94fcaf722d54d7486e365b0468a1ba1d3d3516f8
NitdherG <nitdherdev@gmail.com>
2024-08-05 00:06:57 -0400

    [backend-todo] se agrega configuracion para cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { onRequest } from "firebase-functions/v2/https";
 import express from "express";
+import cors from "cors";
 import userRoutes from "./interfaces/routes/userRoutes";
 import taskRoutes from "./interfaces/routes/taskRoutes";
 
 const app = express();
+// Configurar CORS
+app.use(
+  cors({
+    origin: "http://localhost:4200", 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type"], // Encabezados permitidos
+  }),
+);
 app.use(express.json());
 app.use("/api", taskRoutes);
 app.use("/api", userRoutes);
