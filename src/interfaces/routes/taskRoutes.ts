5f086867cb93d8eca7babf98a8bc425f1b197335
NitdherG <nitdherdev@gmail.com>
2024-08-05 00:07:21 -0400

    [backend-todo] se eliminan rutas que no se utilizan

--- src/interfaces/routes/taskRoutes.ts
+++ src/interfaces/routes/taskRoutes.ts
@@ -4,8 +4,21 @@ import { TaskController } from "../controllers/TaskController";
 const router = Router();
 const taskController = new TaskController();
 
-router.post("/tasks", (req, res) => taskController.addTask(req, res));
-router.get("/tasks", (req, res) => taskController.getTasks(req, res));
-router.delete("/tasks/:id", (req, res) => taskController.deleteTask(req, res));
+/**
+ * Route to delete a specific task for a user.
+ * @route DELETE /users/:email/tasks/:id
+ * @param req - The HTTP request object containing the user's email and task ID as route parameters.
+ * @param res - The HTTP response object.
+ * @returns A response indicating the result of the delete operation.
+ * @remarks
+ * This route invokes the `deleteTask` method of the `TaskController` to delete a task identified by the provided task ID
+ * for the user identified by the provided email.
+ * If the task is successfully deleted, it returns a 200 OK status with a success message.
+ * If the user or task is not found, or if an error occurs, it is handled by the `deleteTask` method of the `TaskController`.
+ */
+router.delete("/users/:email/tasks/:id", (req, res) =>
+  taskController.deleteTask(req, res),
+);
 
 export default router;
+
