8879d086eac89052a3949952d6bc14691ce10588
NitdherG <nitdherdev@gmail.com>
2024-08-05 00:02:16 -0400

    [backend-todo] se agrega comentarios y atributos a las entidades

--- src/domain/entities/Task.ts
+++ src/domain/entities/Task.ts
@@ -1,8 +1,18 @@
+/**
+ * Represents a task with a name, completion status, and creation date.
+ */
 export class Task {
+  /**
+   * Creates an instance of Task.
+   * @param name - The name of the task.
+   * @param completed - The completion status of the task. Defaults to false.
+   * @param createdAt - The date and time when the task was created. Defaults to the current date and time.
+   */
   constructor(
     public id: string,
     public name: string,
     public completed: boolean = false,
-    public createdAt: Date = new Date()
+    public createdAt: Date = new Date(),
+    public description: string = "",
   ) {}
 }
--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,7 +1,15 @@
+/**
+ * Represents a user with an ID, email, and creation date.
+ */
 export class User {
+  /**
+   * Creates an instance of User.
+   * @param id - The unique identifier for the user.
+   * @param email - The email address of the user.
+   * @param createdAt - The date and time when the user was created. Defaults to the current date and time.
+   */
   constructor(
     public id: string,
-    public name: string,
     public email: string,
     public createdAt: Date = new Date(),
   ) {}
