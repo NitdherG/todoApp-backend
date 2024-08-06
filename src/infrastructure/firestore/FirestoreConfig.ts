59cf2c575510966965e7a6434848047c4446cc5b
NitdherG <nitdherdev@gmail.com>
2024-08-06 00:20:52 -0400

    [backend-todo] se cambia la forma en que se implementa la key

--- src/infrastructure/firestore/FirestoreConfig.ts
+++ src/infrastructure/firestore/FirestoreConfig.ts
@@ -9,9 +9,10 @@ if (!serviceAccountPath) {
   throw new Error("GOOGLE_APPLICATION_CREDENTIALS must be set");
 }
 
-const serviceAccount = require("../../../" + serviceAccountPath);
+const serviceAccount = require('../../../todoAppserviceaccount.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+
 });
 const db = admin.firestore();
 export { db, admin };
